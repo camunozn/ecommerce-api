6f5f62f92f5856d5195f6bea3ba6c91960f835f2
camunozn <camunozn89@gmail.com>
2023-04-05 06:04:02 -0500

    Delete unnecesary comments

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,18 +1,6 @@
 const nodemailer = require('nodemailer');
 const htmlToText = require('html-to-text');
 
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.MAILER_USER,
-//     pass: process.env.MAILER_PASSWORD,
-//   },
-// });
-
-// module.exports = transporter;
-
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
